19312996c877a3f3f7b653670053e2c8b7f90494
IntegralTeam <repo@sfxdx.ru>
2020-12-03 16:53:34 +0700

    partly done todos from PoolController

--- contracts/PoolController.sol
+++ contracts/PoolController.sol
@@ -16,7 +16,6 @@ contract PoolController is IPool, Context, Ownable {
         uint256 amount;
         uint256 oracleGasFee;
         uint256 oracleFeeAmount;
-        bool active; // TODO: remove unused
     }
 
     struct RefAccount {
@@ -24,10 +23,10 @@ contract PoolController is IPool, Context, Ownable {
         uint256 bonusPercent;
         uint256 totalWinnings;
         uint256 referralEarningsBalance;
-        uint256 sonCounter; // TODO: rename
+        uint256 referralCounter;
     }
 
-    event RegisteredReferer(address referee, address referral); // TODO: need call that
+    event RegisteredReferer(address referee, address referral);
 
     // referral system
     uint[7] totalWinningsMilestones = [0, 400000, 6000000, 10000000, 14000000, 18000000, 22000000];
@@ -36,39 +35,32 @@ contract PoolController is IPool, Context, Ownable {
     uint256 public constant PERCENT100 = 10 ** 18; // 100 %
 
     // maxbet calc
-    uint256 _gamesCounter; // TODO: refactor all this vars to Poker contract and add private modifier
-    uint256 _betFlip;
-    uint256 _betColor;
-    uint256 _betFlipSquare;
-    uint256 _betColorSquare;
-    uint256 _betFlipVariance;
-    uint256 _betColorVariance;
-    uint256 public maxBet; // TODO: change to private
+    uint256 private _gamesCounter; // TODO: refactor all this vars to Poker contract
+    uint256 private _betFlip;
+    uint256 private _betColor;
+    uint256 private _betFlipSquare;
+    uint256 private _betColorSquare;
+    uint256 private _betFlipVariance;
+    uint256 private _betColorVariance;
+    uint256 private _maxBet;
 
     // jackpot
-    uint256 public jackpot = 500000; // TODO: * 10 ** 18, change to private
-    uint256 internal jackpotLimit = 1000000; // TODO: * 10 ** 18, change to private, add get/set functions for jackpotLimit (set func is onlyOwner)
+    uint256 private _jackpot = 500000; // TODO: ask client
+    uint256 private _jackpotLimit = 1000000; // TODO: ask client, set functions for jackpotLimit (set func is onlyOwner)
 
     mapping (address => RefAccount) refAccounts;
 
-    address _oracleOperator; // TODO: add private modifier
-    IGame private _games; // TODO: rename _games -> _game
-    ERC20 private _tokens;// TODO: remove unused
+    address private _oracleOperator;
+    IGame private _game;
     Pool private pool;
 
     modifier onlyGame() {
-        // bool senderIsAGame = false;
-        require(_msgSender() == address(_games), "caller is not allowed to do some");
+        require(_msgSender() == address(_game), "caller is not allowed to do some");
         _;
     }
 
     modifier onlyOracleOperator() {
         require(_msgSender() == _oracleOperator, "caller is not the operator");
-        _;
-    }
-
-    modifier activePool () { // TODO: remove unused
-        require(pool.active, "pool isn't active");
         _;
     }
 
@@ -79,31 +71,22 @@ contract PoolController is IPool, Context, Ownable {
         require(xEthCandidate.supportsIInternalToken(), "invalid xETH address");
         pool.internalToken = xEthCandidate;
         pool.oracleGasFee = 120000;
-        pool.active = true;
     }
 
-    // function() external payable {
-    //     // require(pool.active, "pool isn't active");
-    //     // _deposit(_msgSender(), msg.value);
-    // }
-
-    //                      Common getters functions                  //
-    // function activateToken(address tokenAddress) external onlyOwner {
-    //     require(tokenAddress != address(0), "invalid token address");
-    //     require(!_pools[tokenAddress].active, "already activated");
-    //     _pools[tokenAddress].active = true;
-    // }
-
-    // function deactivateToken(address tokenAddress) external onlyOwner {
-    //     require(_pools[tokenAddress].active, "already deactivated");
-    //     _pools[tokenAddress].active = false;
-    // }
-    function setOracleGasFee(uint256 oracleGasFee) external onlyOwner {
-        pool.oracleGasFee = oracleGasFee;
+    function getJackpot() external view returns(uint) {
+        return _jackpot;
     }
 
-    function setOracleOperator(address oracleOperator) external onlyOwner {
-        _oracleOperator = oracleOperator;
+    function getJackpotLimit() external view returns(uint) {
+        return _jackpotLimit;
+    }
+
+    function getMaxBet() external view returns(uint) {
+        return (_maxBet); // TODO: ...
+    }
+
+    function getGame() external view returns (address) {
+        return address(_game);
     }
 
     function supportsIPool() external view returns (bool) {
@@ -111,17 +94,15 @@ contract PoolController is IPool, Context, Ownable {
     }
 
     function canWithdraw(uint256 amount) external view returns (uint256) {
-        require(pool.active, "pool isn't active");
         return amount.mul(_getPrice()).div(PERCENT100);
     }
 
-    function getPoolInfo() external view returns (address, uint256, uint256, uint256, bool) {
+    function getPoolInfo() external view returns (address, uint256, uint256, uint256) {
         return (
             address(pool.internalToken),
             pool.amount,
             pool.oracleGasFee,
-            pool.oracleFeeAmount,
-            pool.active
+            pool.oracleFeeAmount
         );
     }
 
@@ -137,30 +118,54 @@ contract PoolController is IPool, Context, Ownable {
         return pool.oracleFeeAmount;
     }
 
-    // TODO: group all onlyGame function to one block
-    function updateJackpot(uint256 amount) external onlyGame activePool() {
-        jackpot = jackpot.add(amount);
+    function _getMyReferralStats(address referee) external view returns (address, uint, uint, uint, uint) {
+        return
+            (
+            refAccounts[referee].parent,
+            refAccounts[referee].bonusPercent,
+            refAccounts[referee].totalWinnings,
+            refAccounts[referee].referralEarningsBalance,
+            refAccounts[referee].referralCounter
+            );
     }
 
-    function jackpotDistribution(address payable player) external onlyGame activePool() returns (bool) {
-        if (jackpot > jackpotLimit) {
-            _rewardDistribution(player, jackpotLimit);
-            jackpot = jackpot.sub(jackpotLimit);
+    function updateJackpot(uint256 amount) external onlyGame {
+        _jackpot = _jackpot.add(amount);
+    }
+
+    function jackpotDistribution(address payable player) external onlyGame returns (bool) {
+        if (_jackpot > _jackpotLimit) {
+            _rewardDistribution(player, _jackpotLimit);
+            _jackpot = _jackpot.sub(_jackpotLimit);
+        } else {
+            _rewardDistribution(player, _jackpot);
+            _jackpot = 0;
         }
-        _rewardDistribution(player, jackpot); // TODO: missing else block
-        jackpot = 0;
-    } // TODO: missing return value
-
-    function updateReferralTotalWinnings(address son, uint amount) external onlyGame activePool() { // TODO: rename son, uint -> uint256
-        address parent = refAccounts[son].parent;
-        refAccounts[parent].totalWinnings = refAccounts[parent].totalWinnings.add(amount);
-        updateReferralBonusRank(parent);
+        return true;
     }
 
-    function updateReferralEarningsBalance(address son, uint amount) external onlyGame activePool() { // TODO: rename son, uint -> uint256
-        address parent = refAccounts[son].parent;
+    function updateReferralTotalWinnings(address referral, uint amount) external onlyGame {
+        address parent = refAccounts[referral].parent;
+        refAccounts[parent].totalWinnings = refAccounts[parent].totalWinnings.add(amount);
+        _updateReferralBonusRank(parent);
+    }
+
+    function updateReferralEarningsBalance(address referral, uint amount) external onlyGame {
+        address parent = refAccounts[referral].parent;
         refAccounts[parent].referralEarningsBalance = refAccounts[parent]
             .referralEarningsBalance.add(amount.mul(refAccounts[parent].bonusPercent));
+    }
+
+    function addBetToPool(uint256 betAmount) external onlyGame payable {
+        uint256 oracleFeeAmount = pool.oracleGasFee;
+        pool.amount = pool.amount.add(betAmount);
+            // .sub(oracleFeeAmount);
+        pool.oracleFeeAmount = pool.oracleFeeAmount.add(oracleFeeAmount);
+    }
+
+    function rewardDisribution(address payable player, uint256 prize) external onlyGame returns (bool) {
+        _rewardDistribution(player, prize);
+        return true;
     }
 
     function withdrawReferralEarnings(address payable player) external {
@@ -169,11 +174,10 @@ contract PoolController is IPool, Context, Ownable {
     }
 
     function deposit(address _to) external payable {
-        // require(msg.value > 0, 'REVERT'); // TODO: remove unused
         _deposit(_to, msg.value);
     }
 
-    function withdraw(uint256 amount) external activePool() {
+    function withdraw(uint256 amount) external {
         require(pool.internalToken.balanceOf(_msgSender()) >= amount, "amount exceeds balance");
         uint256 withdrawAmount = amount.mul(_getPrice()).div(PERCENT100);
         pool.amount = pool.amount.sub(withdrawAmount);
@@ -181,17 +185,21 @@ contract PoolController is IPool, Context, Ownable {
         pool.internalToken.burnTokenFrom(_msgSender(), amount);
     }
 
-    function addBetToPool(uint256 betAmount) external onlyGame activePool() payable {
-        uint256 oracleFeeAmount = pool.oracleGasFee;
-        pool.amount = pool.amount.add(betAmount);
-            // .sub(oracleFeeAmount);
-        pool.oracleFeeAmount = pool.oracleFeeAmount.add(oracleFeeAmount);
-    }
-
-    // TODO: group all onlyOwner functions to one block
     function setGame(address gameAddress) external onlyOwner {
         IGame game = IGame(gameAddress);
-        _games = game;
+        _game = game;
+    }
+
+    function setOracleGasFee(uint256 oracleGasFee) external onlyOwner {
+        pool.oracleGasFee = oracleGasFee;
+    }
+
+    function setOracleOperator(address oracleOperator) external onlyOwner {
+        _oracleOperator = oracleOperator;
+    }
+
+    function setMaxBet(uint256 maxBet) external onlyOwner { // TODO: remove this function ?????????
+        _maxBet = maxBet;
     }
 
     function takeOracleFee() external onlyOracleOperator {
@@ -200,44 +208,17 @@ contract PoolController is IPool, Context, Ownable {
         _msgSender().transfer(oracleFeeAmount);
     }
 
-    // TODO: group all external view functions to one block
-    function getJackpot() public view returns(uint) { // TODO: public -> external
-        return jackpot;
-    }
-
-    function getMaxBet() public view returns(uint) { // TODO: public -> external
-        return (maxBet); // TODO: ...
-    }
-
-    function getGame() public view returns (address) { // TODO: public -> external
-        return address(_games);
-    }
-
-    function setMaxBet(uint256 _maxBet) public { // TODO: remove this function
-        maxBet = _maxBet;
-    }
-
-    function addRef(address parent, address son) public { // TODO: public -> external
+    function addRef(address parent, address son) external {
         require(parent != son, "same address");
         refAccounts[son].parent = parent;
-        refAccounts[parent].sonCounter = refAccounts[parent].sonCounter.add(1);
+        refAccounts[parent].referralCounter = refAccounts[parent].referralCounter.add(1);
+        emit RegisteredReferer(parent, son);
     }
 
-    function getMyReferralStats(address referee) public view returns (address, uint, uint, uint, uint) { // TODO: public -> external, rename getMyReferralStats to getReferralStats
-        return
-            (
-            refAccounts[referee].parent,
-            refAccounts[referee].bonusPercent,
-            refAccounts[referee].totalWinnings,
-            refAccounts[referee].referralEarningsBalance,
-            refAccounts[referee].sonCounter
-            );
-    }
-
-    function maxBetCalc(uint pokerB, uint colorB) public { // TODO: public -> external, uint -> uint256
+    function maxBetCalc(uint pokerB, uint colorB) external {
         _gamesCounter = _gamesCounter.add(1);
         if (_gamesCounter == 1) {
-            if (maxBet == 0) maxBet = pool.amount.div(230);
+            if (_maxBet == 0) _maxBet = pool.amount.div(230);
             _betFlip = pokerB;
             _betColor = colorB;
             _betFlipSquare = pokerB.mul(pokerB);
@@ -254,46 +235,32 @@ contract PoolController is IPool, Context, Ownable {
             uint Cn = _betColor.add(sqrt(_betColorVariance).mul(10));
             if (_gamesCounter > 100 && Fn < pool.amount.div(230) && Cn < pool.amount.div(230)) {
                 if (Fn > Cn) {
-                    maxBet = maxBet.mul(maxBet).div(Fn);
+                    _maxBet = _maxBet.mul(_maxBet).div(Fn);
                 } else {
-                    maxBet = maxBet.mul(maxBet).div(Cn);
+                    _maxBet = _maxBet.mul(_maxBet).div(Cn);
                 }
                 _gamesCounter = 0;
-                if (maxBet > pool.amount.div(109)) maxBet = pool.amount.div(109);
-                else if (maxBet < pool.amount.div(230)) maxBet = pool.amount.div(230);
+                if (_maxBet > pool.amount.div(109)) _maxBet = pool.amount.div(109);
+                else if (_maxBet < pool.amount.div(230)) _maxBet = pool.amount.div(230);
             }
         }
     }
 
-    function rewardDisribution(address payable player, uint256 prize) public onlyGame activePool() returns (bool) { // TODO: public -> external
-        _rewardDistribution(player, prize);
-    } // TODO: missing return value
+    //                      Utility internal functions                      //
+    function _deposit(address staker, uint256 amount) internal {
+        uint256 tokenAmount = amount.mul(PERCENT100).div(_getPrice());
+        pool.amount = pool.amount.add(amount);
+        pool.internalToken.mint(staker, tokenAmount);
+    }
 
-    function updateReferralBonusRank(address parent) internal { // TODO: rename updateReferralBonusRank -> _updateReferralBonusRank
-        uint currentBonus; // TODO: uint -> uint256
+    function _updateReferralBonusRank(address parent) internal {
+        uint currentBonus;
         for (uint i = 0; i < totalWinningsMilestones.length; i++) {
             if (totalWinningsMilestones[i] < refAccounts[parent].totalWinnings) {
                 currentBonus = bonusPercentMilestones[i];
             }
         }
         refAccounts[parent].bonusPercent = currentBonus;
-    }
-
-    function sqrt(uint x) internal pure returns (uint y) { // TODO: move Poker contract, internal -> private, uint -> uint256
-        uint z = (x + 1) / 2;
-        y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
-        }
-    }
-
-    //                      Utility internal functions                      //
-    // TODO: add to this place all internal functions from up side
-    function _deposit(address staker, uint256 amount) internal {
-        uint256 tokenAmount = amount.mul(PERCENT100).div(_getPrice());
-        pool.amount = pool.amount.add(amount);
-        pool.internalToken.mint(staker, tokenAmount);
     }
 
     function _getPrice() internal view returns (uint256) {
@@ -309,5 +276,14 @@ contract PoolController is IPool, Context, Ownable {
         player.transfer(prize);
         pool.amount = pool.amount.sub(prize);
         return true;
+    }
+
+    function sqrt(uint x) private pure returns (uint y) { // TODO: move Poker contract
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
     }
 }
--- test/PoolController.js
+++ test/PoolController.js
@@ -29,12 +29,6 @@ contract('PoolController', async ([owner, alice, bob]) => {
     this.oracle = await Oracle.deployed();
   });
 
-  // it('checking poolController constructor', async () => {
-  //   await expectRevert(PoolController.new(ZERO_ADDRESS, this.xWGR.address, this.xETH.address, { from: owner }), 'invalid iWgr address');
-  //   await expectRevert.unspecified(PoolController.new(this.iWGR.address, ZERO_ADDRESS, this.xETH.address, { from: owner }), '');
-  //   await expectRevert.unspecified(PoolController.new(this.iWGR.address, this.xWGR.address, ZERO_ADDRESS, { from: owner }), '');
-  // });
-
   describe('checking poolController functions:', async () => {
     it('checking supportsIPool function', async () => {
       assert.isTrue(await this.poolController.supportsIPool());
@@ -46,11 +40,11 @@ contract('PoolController', async ([owner, alice, bob]) => {
       assert.equal(ethPoolInfo[1], 0);
       assert.equal(ethPoolInfo[2].toString(), 120000);
       assert.equal(ethPoolInfo[3], 0);
-      assert.equal(ethPoolInfo[4], true);
     });
 
     it('checking setOracleOperator function', async () => {
       assert.equal(await this.poolController.getOracleOperator(), ZERO_ADDRESS);
+      console.log(alice, 12312321);
       await this.poolController.setOracleOperator(alice, { from: owner });
       assert.equal(await this.poolController.getOracleOperator(), getHexAddress(alice));
       simpleExpectRevert(this.poolController.setOracleOperator(owner, { from: alice }), 'not owner');
@@ -64,44 +58,6 @@ contract('PoolController', async ([owner, alice, bob]) => {
       await this.poolController.deposit(owner, { from: owner, callValue: 1000 });
       assert.equal(await this.xETH.balanceOf(owner, { from: owner }), 1000);
     });
-
-
-    // it('checking setGame function', async () => {
-    //   this.russianRoulette = await RussianRoulette.new(this.oracle.address, this.poolController.address);
-    //   await this.poolController.setGame(this.russianRoulette.address, { from: owner });
-    //   expect(await this.poolController.getGamesCount()).to.be.bignumber.equal(new BN(1));
-    //   await expectRevert.unspecified(this.poolController.addGame(this.xETH.address, { from: owner }), '');
-    // });
-
-    // it('checking getGame function', async () => {
-    //   await expectRevert(this.poolController.getGame(new BN('0')), 'index out of range');
-    //   this.russianRoulette = await RussianRoulette.new(this.oracle.address, this.poolController.address);
-    //   await this.poolController.addGame(this.russianRoulette.address, { from: owner });
-    //   const game = await this.poolController.getGame(new BN('0'));
-    //   expect(await this.russianRoulette.address).to.equal(game);
-    // });
-
-    // it('should can not call addBetToPool & rewardDisribution functions directly (only game contract)', async () => {
-    //   await expectRevert(this.poolController.addBetToPool(ETH_ADDRESS, getBNEth('1')), 'caller is not allowed to do some');
-    //   await expectRevert(this.poolController.rewardDisribution(holder, ETH_ADDRESS, getBNEth('1')), 'caller is not allowed to do some');
-    // });
-
-    // it('checking maxBet function', async () => {
-    //   expect(await this.poolController.maxBet(ETH_ADDRESS, new BN('1000000000000000001'))).to.be.bignumber.equal(new BN('0')); // > 100%, 10**18 + 1
-    //   expect(await this.poolController.maxBet(ETH_ADDRESS, new BN('500000000000000000'))).to.be.bignumber.equal(new BN('0')); // 50%, pool is empty
-    //   await this.xETH.setPoolController(this.poolController.address, { from: owner });
-    //   await this.poolController.depositToken(ETH_ADDRESS, getBNEth('1'), { from: holder, value: getBNEth('1') });
-    //   expect(await this.poolController.maxBet(ETH_ADDRESS, new BN('500000000000000000'))).to.be.bignumber.equal(getBNEth('0.5')); // 50%
-    //   await this.poolController.deactivateToken(ETH_ADDRESS, { from: owner });
-    //   expect(await this.poolController.maxBet(ETH_ADDRESS, new BN('500000000000000000'))).to.be.bignumber.equal(new BN('0')); // pool is deactivate
-    // });
-
-    // it('checking canWithdraw function', async () => {
-    //   await expectRevert(this.poolController.canWithdraw(this.xWGR.address, getBNEth('1')), 'pool isn\'t active');
-    //   await expectRevert(this.poolController.canWithdraw(owner, getBNEth('1')), 'pool isn\'t active');
-    //   expect(await this.poolController.canWithdraw(this.iWGR.address, getBNEth('1'))).to.be.bignumber.equal(getBNEth('1'));
-    //   expect(await this.poolController.canWithdraw(ETH_ADDRESS, getBNEth('1'))).to.be.bignumber.equal(getBNEth('1'));
-    // });
 
     describe('checking withdraw functions:', async () => {
       beforeEach(async () => {
