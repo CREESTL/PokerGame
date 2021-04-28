2d4dcd60bed75f6e460c45f9e0c7e8b2482a38a9
IntegralTeam <repo@sfxdx.ru>
2021-04-28 16:29:19 +0700

    updated maxBetCalc, added JS maxBetCalc checker

--- test/CheckMaxBet.js
+++ test/CheckMaxBet.js
@@ -1,3 +1,4 @@
+// thanks to tronbox and tvm this test is completely useless
 const { getHexAddress, simpleExpectRevert } = require('./utils/tronBSHelpers');
 const wait = require('./helpers/wait')
 const chalk = require('chalk')
@@ -8,22 +9,12 @@ require('chai')
 
 const XETHToken = artifacts.require('XETHToken');
 const PoolController = artifacts.require('PoolController');
-const GameController = artifacts.require('GameController');
 const Oracle = artifacts.require('Oracle');
-const FakePoker = artifacts.require('FakePoker');
+const Poker = artifacts.require('Poker');
 
 
 
 const userWinsCards = [11, 10, 22, 47, 7, 5, 2, 1, 3];
-const drawCards = [0, 1, 12, 25, 38, 51, 13, 14, 26];
-const computerWinsCards = [1, 2, 4, 6, 8, 9, 10, 11, 12];
-const userWinsJackpotCards = [12, 11, 10, 9, 8, 5, 2, 1, 3];
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const checkingToBitArr = [1, 6, 13, 14, 24, 27, 44, 45, 50];
-const checkingToBitInt = 14274713982914945;
-
-const evenWinColorCards = [27, 1, 42]; // returns 2, 0, 3 even wins
-const oddWinColorCards = [22, 47, 7]; // retruns 1, 3, 0 odd wins
 
 contract('MaxBetCalc test', async ([owner, alice, bob]) => {
 
@@ -31,38 +22,28 @@ contract('MaxBetCalc test', async ([owner, alice, bob]) => {
     this.xETH = await XETHToken.deployed();
     this.poolController = await PoolController.deployed();
     this.oracle = await Oracle.deployed();
-    this.fakePoker = await FakePoker.deployed();
+    this.poker = await Poker.deployed();
 
     await this.xETH.setPoolController(this.poolController.address, { from: owner });
     assert.equal(await this.xETH.getPoolController({ from: owner }),(this.poolController.address));
-    await this.poolController.setGame(this.fakePoker.address, { from: owner });
-    await this.oracle.setGame(this.fakePoker.address, { from: owner });
+    await this.poolController.setGame(this.poker.address, { from: owner });
+    await this.oracle.setGame(this.poker.address, { from: owner });
+
+    assert.equal(await this.xETH.balanceOf(owner, { from: owner }), 0);
+    await this.poker.setMaxBet(1000000000);
+    await this.poolController.deposit(owner, { from: owner, callValue: 5000000000 });
+    await this.poolController.deposit(owner, { from: bob, callValue: 5000000000 });
+    assert.equal((await this.xETH.balanceOf(owner, { from: owner })).toString(), 10000000000);
+    assert.equal(((await this.poolController.getPoolInfo())[1]).toString(), 10000000000);
   });
 
-  it('checking maxBetCalc', async () => {
-    assert.equal(await this.xETH.balanceOf(owner, { from: owner }), 0);
-    await this.poolController.deposit(owner, { from: owner, callValue: 1000000 });
-    assert.equal(await this.xETH.balanceOf(owner, { from: owner }), 1000000);
-    // assert.equal((await this.poolController.getMaxBet({ from: owner })).toString(), 0);
-    // await this.poolController.maxBetCalc(10000, 10000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 0);
-    assert.equal(await this.xETH.balanceOf(owner, { from: owner }), 1000000);
-    await this.poolController.deposit(owner, { from: owner, callValue: 1000000000 });
-   
-    await this.fakePoker.maxBetCalc(10000, 10000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 4352173);
-    await this.fakePoker.maxBetCalc(5000, 5000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 4352173);
-    await this.fakePoker.maxBetCalc(3000, 3000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 9183486);
-    await this.fakePoker.maxBetCalc(1000, 1000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 9183486);
-    await this.poolController.deposit(owner, { from: owner, callValue: 1000000000 });
-    await this.fakePoker.maxBetCalc(10000, 10000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 9183486);
-    await this.fakePoker.maxBetCalc(10000, 10000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 18357798);
-    await this.fakePoker.maxBetCalc(10000, 10000);
-    assert.equal((await this.fakePoker.getMaxBet({ from: owner })).toString(), 18357798);
+  it('checking maxBetCalc', async () => {      
+    for (let i = 0; i < 102; i++) {
+      await this.poker.play(0, 0, { from: alice, callValue: 50000000 });
+      const poolInfo = await this.poolController.getPoolInfo();
+      const requestId = await this.poker.getLastRequestId();
+      await this.oracle.publishRandomNumber(userWinsCards, this.poker.address, requestId, { from: owner });
+      console.log(poolInfo[1].toString(), i);
+    }
   })
 });
