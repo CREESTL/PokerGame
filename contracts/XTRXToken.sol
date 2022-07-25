2dec1453996977c5aefd4b5aae931a9ab6a0be82
IntegralTeam <sfxdx@sfxdx.com>
2022-07-25 16:18:09 +0300

    Delete Return Value From `rewardDistribution`. Add Comments.

--- contracts/XTRXToken.sol
+++ contracts/XTRXToken.sol
@@ -9,67 +9,74 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./Interfaces.sol";
 
 
-contract XTRXToken is IERC20Metadata, IInternalToken, ERC20Burnable, Ownable {
+contract XTRXToken is ERC20Burnable, IInternalToken, Ownable {
     using SafeMath for uint256;
 
-//    mapping (address => bool) internal _burnerAddresses; // TODO: remove all unused code
+    /// @dev Only some addresses can burn tokens
+    mapping (address => bool) internal _burnerAddresses; 
     address internal _poolController;
 
-//    modifier isBurner() {
-//        require(_burnerAddresses[_msgSender()], "Caller is not burner");
-//        _;
-//    }
+    /// @dev Checks if caller is allowed to burn tokens
+    modifier isBurner() {
+       require(_burnerAddresses[_msgSender()], "Caller is not allowed to burn tokens!");
+       _;
+    }
 
+    /// @dev Checks if caller controls the pool
     modifier onlyPoolController() {
-        require(_msgSender() == _poolController, "Caller is not pool controller");
+        require(_msgSender() == _poolController, "Caller is not a pool controller!");
         _;
     }
 
-    constructor() ERC20("xEthereum", "xTRX", 18) public {
-//        _burnerAddresses[_msgSender()] = true;
+    /// @dev Create a new token
+    /// @dev Add the caller of constructor to the list of burners
+    constructor() ERC20("xEthereum", "xTRX", 18) {
+        _burnerAddresses[_msgSender()] = true;
     }
 
+
+    /// @dev Checks if this contract supports interface
     function supportsIInternalToken() external view returns (bool) {
         return true;
     }
 
+
+    /// @dev Getter for a pool controller address
     function getPoolController() external view returns (address) {
         return _poolController;
     }
 
+    /// @dev Setter for a pool controller address
     function setPoolController(address poolControllerAddress) external onlyOwner {
         IPool iPoolCandidate = IPool(poolControllerAddress);
-        require(iPoolCandidate.supportsIPool(), "Invalid IPool address");
+        require(iPoolCandidate.supportsIPool(), "Invalid IPool address!");
         _poolController = poolControllerAddress;
     }
 
-//    function isBurnAllowed(address account) external view returns(bool) {
-//        return _burnerAddresses[account];
-//    }
-//
-//    function addBurner(address account) external onlyOwner {
-//        require(!_burnerAddresses[account], "Already burner");
-//        require(account != address(0), "Cant add zero address");
-//        _burnerAddresses[account] = true;
-//    }
-//
-//    function removeBurner(address account) external onlyOwner {
-//        require(_burnerAddresses[account], "Isnt burner");
-//        delete  _burnerAddresses[account];
-//    }
+    /// @dev Checks if a provided user is allowes to burn tokens
+    function isBurnAllowed(address account) external view returns(bool) {
+       return _burnerAddresses[account];
+    }
 
+    /// @dev Allows a provided user to burn tokens
+    function addBurner(address account) external onlyOwner {
+       require(!_burnerAddresses[account], "This user is already allowed to burn tokens!");
+       require(account != address(0), "Can't allow a zero address to burn tokens!");
+       _burnerAddresses[account] = true;
+    }
+
+    /// @dev Prohibits the user from burning tokens
+    function removeBurner(address account) external onlyOwner {
+       require(_burnerAddresses[account], "Isnt burner");
+       delete  _burnerAddresses[account];
+    }
+
+    /// @dev Pool controller can mint tokens to anyone
     function mint(address recipient, uint256 amount) external onlyPoolController {
         _mint(recipient, amount);
     }
 
-//    function burn(uint256 amount) external isBurner {
-//        _burn(_msgSender(), amount);
-//    }
-//
-//    function burnFrom(address account, uint256 amount) external isBurner {
-//        super.burnFrom(account, amount);
-//    }
-
+    /// @dev Pool controller can burn anyone's tokens
     function burnTokenFrom(address account, uint256 amount) external onlyPoolController {
         _burn(account, amount);
     }
